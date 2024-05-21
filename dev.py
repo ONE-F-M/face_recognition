e059f49ce3a26a0704988e5dc18e829d1f3df441
Anthony C. Emmanuel <mymi14s@hotmail.com>
2024-05-21 12:31:21 +0100

    added: dev.log

--- dev.py
+++ dev.py
@@ -1,7 +1,7 @@
 """Gunicorn *development* config file"""
 
 # Flask WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
-swgi_app = "app:app"
+wsgi_app = "app:app"
 # The granularity of Error log outputs
 loglevel = "debug"
 # The number of worker processes for handling requests
