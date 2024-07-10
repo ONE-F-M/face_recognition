f79a7b7fa9ef19c0bed133421e973ab55c3ecf8a
abraham_adekunle <gadekunle@afexnigeria.com>
2024-07-10 15:56:38 +0100

    removed comment

--- face_engine.py
+++ face_engine.py
@@ -516,10 +516,10 @@ class FaceRecognition:
             self.get_path()
             video_path = self.save_video()
 
-            #status, message, traceback = self.anti_spoof_liveliness(file_path=video_path)
-            #if not status:
-            #    os.remove(video_path) if os.path.isfile(video_path) else None
-            #    return True, message, traceback
+            status, message, traceback = self.anti_spoof_liveliness(file_path=video_path)
+            if not status:
+               os.remove(video_path) if os.path.isfile(video_path) else None
+               return True, message, traceback
 
             cap = cv2.VideoCapture(video_path)
 
