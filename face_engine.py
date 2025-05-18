eeca8535468836c36f187d5ca4e83381fce69eca
staging <staging_user@one-fm.com>
2025-05-18 13:57:25 +0000

    Added Debug Logs

--- face_engine.py
+++ face_engine.py
@@ -502,7 +502,7 @@ class FaceRecognition:
             unmatched_count = 0
             checkin_images = [os.path.join(checkin_image_folder, img) for img in os.listdir(checkin_image_folder) if img.lower().endswith(('.jpg', '.jpeg', '.png'))]
             enrollment_images = [os.path.join(enrollment_image_folder, img) for img in os.listdir(enrollment_image_folder) if img.lower().endswith(('.jpg', '.jpeg', '.png'))]
-            
+            logging.debug(f"USER:{self._username}")
             for checkin_image in checkin_images:
                 if match_count>9:
                     break 
@@ -529,7 +529,7 @@ class FaceRecognition:
             
             os.remove(video_path) if os.path.isfile(video_path) else None
             shutil.rmtree(checkin_image_folder, ignore_errors=True) if os.path.exists(checkin_image_folder) else None
-            
+            logging.debug(f"FINAL RESULTS: MATCH: {match_count} UNMATCHED: {unmatched_count}")
             if match_count >= unmatched_count:
                 return False, "Face verification Successful", ""
             else:
